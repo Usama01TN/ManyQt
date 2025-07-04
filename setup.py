7ba954298c05e9511b31a8058e98f207ff230c30
UsamaAininou <100040018+Usama01TN@users.noreply.github.com>
2025-07-04 13:59:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@
 Setup script for ManyQt.
 """
 from setuptools import setup, find_packages
+from os.path import exists, join, dirname
 import io
 
 NAME = "ManyQt"  # type: str
-VERSION = "0.3.0"  # type: str
+VERSION = "0.3.2"  # type: str
 DEVELOPER = "UsamaTN"  # type: str
 DEVELOPER_EMAIL = "ininou.oussematn@gmail.com"  # type: str
 PACKAGES = find_packages(".", include="ManyQt*")  # type: list[str]
-DESCRIPTION = "PyQt/PyQt compatibility layer."  # type: str
-with io.open("README.txt", encoding="utf-8") as f:
+DESCRIPTION = "PyQt/PySide compatibility layer."  # type: str
+with io.open("README.rst" if exists("README.rst") else join(dirname(__file__), "README.rst"), encoding="utf-8") as f:
     README = f.read()  # type: str
 LICENSE = "GPLv3"  # type: str
 CLASSIFIERS = [
@@ -22,7 +23,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 2",
     "Programming Language :: Python :: 3",
 ]  # type: list[str]
-KEYWORDS = ["GUI", "PyQt4", "PyQt5", "PySide", "PySide2", "compatibility"]  # type: list[str]
+KEYWORDS = ["GUI", "PyQt4", "PyQt5", "PyQt6", "PySide", "PySide2", "PySide6", "compatibility"]  # type: list[str]
 
 if __name__ == "__main__":
     setup(name=NAME, version=VERSION, author=DEVELOPER, description=DESCRIPTION, long_description=README,
